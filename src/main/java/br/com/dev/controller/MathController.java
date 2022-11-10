73f0684bde49385bd752f8f21e0f6db494b57d99
rodrigoqueiroz01 <rodrigoq1212@gmail.com>
2022-11-10 13:34:25 -0300

    Calculator Math

--- src/main/java/br/com/dev/controller/MathController.java
+++ src/main/java/br/com/dev/controller/MathController.java
@@ -3,10 +3,8 @@ package br.com.dev.controller;
 import br.com.dev.converter.NumberConverter;
 import br.com.dev.exception.UnsupportedMathOperationException;
 import br.com.dev.math.SimpleMath;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -17,7 +15,7 @@ public class MathController {
 
     private SimpleMath math = new SimpleMath();
 
-    @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    @GetMapping("/sum/{numberOne}/{numberTwo}")
     public String sum(@PathVariable String numberOne, @PathVariable String numberTwo) throws Exception {
         if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new UnsupportedMathOperationException("Please set a numeric value!");
@@ -28,7 +26,7 @@ public class MathController {
         return "Sum: " + NumberConverter.convertToDouble(numberOne) + " + " + NumberConverter.convertToDouble(numberTwo) + " = " + result;
     }
 
-    @RequestMapping(value = "/subtraction/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    @GetMapping("/subtraction/{numberOne}/{numberTwo}")
     public String subtraction(@PathVariable(value = "numberOne") String numberOne,
                               @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
         if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
@@ -40,7 +38,7 @@ public class MathController {
         return "Subtraction: " + NumberConverter.convertToDouble(numberOne) + " - " + NumberConverter.convertToDouble(numberTwo) + " = " + result;
     }
 
-    @RequestMapping(value = "/multiplication/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    @GetMapping("/multiplication/{numberOne}/{numberTwo}")
     public String multiplication(@PathVariable(value = "numberOne") String numberOne,
                                  @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
         if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
@@ -52,7 +50,7 @@ public class MathController {
         return "Multiplication: " + NumberConverter.convertToDouble(numberOne) + " * " + NumberConverter.convertToDouble(numberTwo) + " = " + result;
     }
 
-    @RequestMapping(value = "/division/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    @GetMapping("/division/{numberOne}/{numberTwo}")
     public String division(@PathVariable(value = "numberOne") String numberOne,
                            @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
         if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
@@ -64,7 +62,7 @@ public class MathController {
         return "Division: " + NumberConverter.convertToDouble(numberOne) + " / " + NumberConverter.convertToDouble(numberTwo) + " = " + result;
     }
 
-    @RequestMapping(value = "/mean/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    @GetMapping("/mean/{numberOne}/{numberTwo}")
     public String mean(@PathVariable(value = "numberOne") String numberOne,
                        @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
         if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
@@ -76,7 +74,7 @@ public class MathController {
         return "Mean: (" + NumberConverter.convertToDouble(numberOne) + " + " + NumberConverter.convertToDouble(numberTwo) + ") / 2 = " + result;
     }
 
-    @RequestMapping(value = "/squareRoot/{number}", method = RequestMethod.GET)
+    @GetMapping("/squareRoot/{numberOne}/{numberTwo}")
     public String squareRoot(@PathVariable(value = "number") String number) throws Exception {
         if (!NumberConverter.isNumeric(number)) {
             throw new UnsupportedMathOperationException("Please set a numeric value!");
